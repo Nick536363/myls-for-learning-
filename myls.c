4909f5b72c33911f884d8ad10f87bd7863e27f87
Nick536363 <3383371nik@gmail.com>
2026-05-21 19:14:33 +0200

    moved realisation of all functions into separate file

--- myls.c
+++ myls.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <dirent.h>
-#include <errno.h>
-#include <string.h>
 #include "myls.h"
-#include <stdlib.h>
-#include <sys/stat.h>
+
 
 FLAGS flags = {};
 
@@ -66,44 +61,3 @@ int main(int argc, char** argv)
 
 
 
-void set_flags(FLAGS* flags, const char* args)
-{
-	for(int flag = 1; flag < strlen(args); flag++)
-		switch(args[flag]){
-			case 'a':
-				flags->show_hidden = 1;
-				break;
-			case 'l':
-				flags->ext_info = 1;
-				break;
-			case 'f':
-				flags->full_path = 1;
-				break;
-		}
-}
-
-
-void print_file(const char* filename, struct stat* file_info){
-    if(S_ISDIR(file_info->st_mode))
-			printf("\033[1;34m%s\033[1;0m", filename);
-	else if( (file_info->st_mode & S_IXUSR ) && (file_info->st_mode & S_IXGRP) && (file_info->st_mode & S_IXOTH) )
-		printf("\033[1;35m%s\033[1;0m", filename);
-	else if(file_info->st_mode & (S_IXUSR))
-		printf("\033[1;32m%s\033[1;0m", filename);
-	else
-		printf("%s", filename);
-}
-
-
-void print_extra(struct stat* file_info)
-{
-	int permissions[] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
-	printf("%zu\t", file_info->st_size);
-	if(S_ISDIR(file_info->st_mode))
-		putchar('d');
-	else
-		putchar('-');
-	for(int i = 0; i <= 8; i++)
-		putchar(file_info->st_mode & (permissions[i]) ? "rwxrwxrwx"[i] : '-');
-	putchar('\t');
-}
--- myls.h
+++ myls.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <sys/stat.h>
+
 typedef struct 
 {
 	unsigned show_hidden: 1;
@@ -7,6 +7,4 @@ typedef struct
 	unsigned full_path: 1;
 } FLAGS;
 
-void set_flags(FLAGS*, const char*);
-void print_file(const char*, struct stat*);
-void print_extra(struct stat*);
+#include "mylib.c"
